80428a74c7b08188541ce7e257bcad76d7e55141
daria <dasha_domracheva@mail.ru>
2018-10-17 13:35:42 +0300

    17.10.18 13:35 added counters

--- task2/blocks.cpp
+++ task2/blocks.cpp
@@ -1,13 +1,16 @@
 //g++ -I/usr/local/include -O0 blocks.cpp /usr/local/lib/libpapi.a -o blocks
-// ./multiply A.dat B.dat C.dat size_of_block
+// ./blocks A.dat B.dat C.dat size_of_block
 #include <iostream>
 #include <fstream>
 #include <ctime>
 #include <cstring>
 #include <inttypes.h>
 #include <papi.h>
+#include <vector>
+#include <papiStdEventDefs.h>
 
-#define NUMBER_OF_EVENTS 6
+#define NUMBER_OF_COUNTERS 7
+#define NUMBER_OF_ARGC 6
 
 using namespace std;
 
@@ -33,7 +36,7 @@ void write_matrix(float ** M, uint64_t n, uint64_t m, fstream & s) {
         for (uint64_t j = 0; j < m; j++) 
             s.write((char *)&(M[i][j]), sizeof(float));   
 }
-uint64_t multiply(float ** A, float ** B, float ** C, uint64_t n, uint64_t m, 
+uint64_t multiply_ijk(float ** A, float ** B, float ** C, uint64_t n, uint64_t m, 
                     uint64_t l, uint64_t size_of_block, fstream & f) {
 	uint64_t time_mul = clock();
 	for (uint64_t i = 0; i < n; i += size_of_block) 
@@ -45,7 +48,19 @@ uint64_t multiply(float ** A, float ** B, float ** C, uint64_t n, uint64_t m,
 							C[i1][j1] += A[i1][k1] * B[k1][j1];
 	return clock() - time_mul;						
 }
-double get_time(char * a, char * b, char * c, uint64_t size_of_block) {
+uint64_t multiply_ikj(float ** A, float ** B, float ** C, uint64_t n, uint64_t m, 
+                    uint64_t l, uint64_t size_of_block, fstream & f) {
+	uint64_t time_mul = clock();
+	for (uint64_t i = 0; i < n; i += size_of_block) 
+		for (uint64_t k = 0; k < l; k += size_of_block) 
+			for (uint64_t j = 0; j < m; j += size_of_block)
+				for (uint64_t i1 = i; i1 < i + size_of_block && i1 < n; i1++)
+					for (uint64_t k1 = k; k1 < k + size_of_block && k1 < l; k1++)
+						for (uint64_t j1 = j; j1 < j + size_of_block && j1 < m; j1++)
+							C[i1][j1] += A[i1][k1] * B[k1][j1];
+	return clock() - time_mul;						
+}
+double get_time(char * a, char * b, char * c, char mode, uint64_t size_of_block) {
 	fstream a_f, b_f;
     a_f.open(a, ios::binary | ios::in);
     b_f.open(b, ios::binary | ios::in);
@@ -62,32 +77,126 @@ double get_time(char * a, char * b, char * c, uint64_t size_of_block) {
 	read_matrix(B, N_b, M_b, b_f);
 	float ** C = init_matrix(N_a, M_b); 
 	uint64_t time_mul; 
-	time_mul = multiply(A, B, C, N_a, M_b, N_b, size_of_block, c_f);
+	if (mode == 0) time_mul = multiply_ijk(A, B, C, N_a, M_b, N_b, size_of_block, c_f);
+    else  time_mul = multiply_ikj(A, B, C, N_a, M_b, N_b, size_of_block, c_f);
 	delete_matrix(A, N_a);
 	delete_matrix(B, N_b);
 	delete_matrix(C, N_a);
 	return time_mul/(double)CLOCKS_PER_SEC;
 }
-
 void handle_error(int err){
     std::cerr << "PAPI error: " << err << std::endl;
 }
 
+// ./blocks A.dat B.dat C.dat {0 - ijk, 1 - ikj} size_of_block params
+//NO FLOPS COUNTER
 int main(int argc, char ** argv) {
-	if (argc != 5) {
-        cerr << "Wrong number of arguments";
+    if (argc == 2) {
+        if (strcmp(argv[1], "help") == 0 || strcmp(argv[1], "h") == 0) {
+            cout << "help information: " << endl;
+            cout << "Modes:\n1 - PAPI_L1_TCM\n2 - PAPI_L2_TCM\n3 - PAPI_L3_TCM\n";
+            cout << "4 - PAPI_TOT_INS\n5 - PAPI_TLB_IM\n6 - PAPI_TOT_CYC\n7 - PAPI_TLB_DM\n";
+            return 0;
+        } else {
+            cerr << "Invalid command line" << endl;
+            return -1;
+        }
+    }
+	if (argc < NUMBER_OF_ARGC) {
+        cerr << "Wrong number of arguments" << endl;
         return -1;
     }
-    long long values[NUMBER_OF_EVENTS];
-    int events[NUMBER_OF_EVENTS] = {PAPI_L1_DCM, PAPI_L1_ICM, PAPI_L2_TCA, PAPI_L2_TCM, PAPI_L3_TCA, PAPI_L3_TCM};
-
+    int mode;
 	uint64_t size_of_block;
-	sscanf(argv[4], "%", SCNd64, &size_of_block); // change format
+    sscanf(argv[4], "%d", &mode);
+	sscanf(argv[5], "%", SCNd64, &size_of_block); 
+    if ((mode != 0 && mode != 1) || size_of_block < 1) {
+        cerr << "Invalid parameters" << endl;
+        return -1;
+    }
     double time_mul;
-    if (PAPI_start_counters(events, NUMBER_OF_EVENTS) != PAPI_OK)
-        handle_error(1);
+    int NUMBER_OF_EVENTS, retval;
+    long long * values;
+    int COUNTERS[NUMBER_OF_COUNTERS];
+    int * events;
+    vector<string> NAMES;
+    if (argc - NUMBER_OF_ARGC > 0) { //there are counters to compute
+        if ((retval = PAPI_library_init(PAPI_VER_CURRENT)) != PAPI_VER_CURRENT) {
+            handle_error(retval);
+        }
+        NUMBER_OF_EVENTS = argc - NUMBER_OF_ARGC;
+        values = new long long[NUMBER_OF_EVENTS];
+        memset(COUNTERS, 0, sizeof(int) * NUMBER_OF_COUNTERS);
+        events = new int[NUMBER_OF_EVENTS];
+        // MAKE A MAP OUT OF THIS?
+        for (int i = NUMBER_OF_ARGC; i < argc; i++) {
+            int counter;
+            sscanf(argv[i], "%d", &counter);
+            bool flag = false;
+            switch (counter) {
+                case 1:
+                    if (COUNTERS[0] == 0) {
+                        events[i - NUMBER_OF_ARGC] = PAPI_L1_TCM;
+                        NAMES.push_back("Level 1 cache misses");
+                        COUNTERS[0]++;
+                    } else flag = true;
+                    break;
+                case 2:
+                    if (COUNTERS[1] == 0) {
+                        events[i - NUMBER_OF_ARGC] = PAPI_L2_TCM;
+                        NAMES.push_back("Level 2 cache misses");
+                        COUNTERS[1]++;
+                    } else flag = true;
+                    break;
+                case 3:
+                    if (COUNTERS[2] == 0) {
+                        events[i - NUMBER_OF_ARGC] = PAPI_L3_TCM;
+                        NAMES.push_back("Level 3 cache misses");
+                        COUNTERS[2]++;
+                    } else flag = true;
+                    break;
+                case 4:
+                    if (COUNTERS[3] == 0) { 
+                        events[i - NUMBER_OF_ARGC] = PAPI_TOT_INS;
+                        NAMES.push_back("Instructions completed");
+                        COUNTERS[3]++;
+                    } else flag = true;
+                    break;
+                case 5:
+                    if (COUNTERS[4] == 0) {
+                        events[i - NUMBER_OF_ARGC] = PAPI_TLB_IM;
+                        NAMES.push_back("Instruction translation lookaside buffer misses");
+                        COUNTERS[4]++;
+                    } else flag = true;
+                    break;
+                case 6:
+                    if (COUNTERS[5] == 0) {
+                        events[i - NUMBER_OF_ARGC] = PAPI_TOT_CYC;
+                        NAMES.push_back("Total cycles");
+                        COUNTERS[5]++;
+                    } else flag = true;
+                    break;
+                case 7:
+                    if (COUNTERS[6] == 0) {
+                        events[i - NUMBER_OF_ARGC] = PAPI_TLB_DM;
+                        NAMES.push_back("Data translation lookaside buffer misses");
+                        COUNTERS[6]++;
+                    } else flag = true;
+                    break;
+                default :
+                    cerr << "Invalid counter" << endl;
+                    return -1;
+            }
+            if (flag) {
+                cerr << "Invalid counters list" << endl;
+                    return -1;
+            }
+        }
+        if ((retval = PAPI_start_counters(events, NUMBER_OF_EVENTS)) != PAPI_OK)
+            handle_error(retval);
+    }
     try{
-        time_mul = get_time(argv[1], argv[2], argv[3], size_of_block);
+        time_mul = get_time(argv[1], argv[2], argv[3], mode, size_of_block);
         if (time_mul == -1) throw "Invalid data caused an error";
     } catch (const char * s) {
         cerr << s;
@@ -95,20 +204,18 @@ int main(int argc, char ** argv) {
     } catch (exception &e) {
         cerr << e.what();
     } catch (...) {
-        cerr << "An error occured";
+        cerr << "An error occured" << endl;
         return -1;
     }	
+    if (argc - NUMBER_OF_ARGC > 0) {
+        if ((retval = PAPI_stop_counters(values, NUMBER_OF_EVENTS))!= PAPI_OK)
+            handle_error(retval);
+        for (int i = 0; i < NUMBER_OF_EVENTS; i++) {
+            cout << NAMES[i] << ": " << values[i] << endl;
+        }
+        delete []values;
+        delete []events;
+    }
 	cout << "Time: " << time_mul << endl;
-
-      if ( PAPI_stop_counters(values, NUMBER_OF_EVENTS) != PAPI_OK)
-        handle_error(1);
-    cout << "L1 data cache misses: " << values[0] << endl;
-    cout << "L1 instruction cache misses: " << values[1] << endl;
-    cout << "L2 accesses: " << values[2] << endl;
-    cout << "L2 misses: " << values[3] << endl;
-    cout << "L2 miss/access ratio: " << (double)values[3]/values[2] << endl;
-    cout << "L3 accesses: " << values[4] << endl;
-    cout << "L3 misses: " << values[5] << endl;
-    cout << "L3 miss/access ratio: " << (double)values[5]/values[4] << endl;
 	return 0;
 }
