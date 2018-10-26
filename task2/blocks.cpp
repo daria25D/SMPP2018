849847d76fce151da666b302d4f9f2b54037f914
daria25D <43067050+daria25D@users.noreply.github.com>
2018-10-26 15:29:01 +0300

    spaces

--- task2/blocks.cpp
+++ task2/blocks.cpp
@@ -15,10 +15,10 @@
 using namespace std;
 
 float ** init_matrix(uint64_t n, uint64_t m) {
-	float ** M = new float *[n];
-	for (uint64_t i = 0; i < n; i++)
-		M[i] = new float[m];
-	return M;	
+    float ** M = new float *[n];
+    for (uint64_t i = 0; i < n; i++)
+	M[i] = new float[m];
+    return M;	
 }
 void delete_matrix(float ** M, uint64_t rows) {
     for (uint64_t i = 0; i < rows; i++) {
@@ -38,30 +38,30 @@ void write_matrix(float ** M, uint64_t n, uint64_t m, fstream & s) {
 }
 uint64_t multiply_ijk(float ** A, float ** B, float ** C, uint64_t n, uint64_t m, 
                     uint64_t l, uint64_t size_of_block, fstream & f) {
-	uint64_t time_mul = clock();
-	for (uint64_t i = 0; i < n; i += size_of_block) 
-		for (uint64_t j = 0; j < m; j += size_of_block) 
-			for (uint64_t k = 0; k < l; k += size_of_block)
-				for (uint64_t i1 = i; i1 < i + size_of_block && i1 < n; i1++)
-					for (uint64_t j1 = j; j1 < j + size_of_block && j1 < m; j1++)
-						for (uint64_t k1 = k; k1 < k + size_of_block && k1 < l; k1++)
-							C[i1][j1] += A[i1][k1] * B[k1][j1];
-	return clock() - time_mul;						
+    uint64_t time_mul = clock();
+    for (uint64_t i = 0; i < n; i += size_of_block) 
+	for (uint64_t j = 0; j < m; j += size_of_block) 
+	    for (uint64_t k = 0; k < l; k += size_of_block)
+		for (uint64_t i1 = i; i1 < i + size_of_block && i1 < n; i1++)
+                    for (uint64_t j1 = j; j1 < j + size_of_block && j1 < m; j1++)
+			for (uint64_t k1 = k; k1 < k + size_of_block && k1 < l; k1++)
+			    C[i1][j1] += A[i1][k1] * B[k1][j1];
+    return clock() - time_mul;						
 }
 uint64_t multiply_ikj(float ** A, float ** B, float ** C, uint64_t n, uint64_t m, 
                     uint64_t l, uint64_t size_of_block, fstream & f) {
-	uint64_t time_mul = clock();
-	for (uint64_t i = 0; i < n; i += size_of_block) 
-		for (uint64_t k = 0; k < l; k += size_of_block) 
-			for (uint64_t j = 0; j < m; j += size_of_block)
-				for (uint64_t i1 = i; i1 < i + size_of_block && i1 < n; i1++)
-					for (uint64_t k1 = k; k1 < k + size_of_block && k1 < l; k1++)
-						for (uint64_t j1 = j; j1 < j + size_of_block && j1 < m; j1++)
-							C[i1][j1] += A[i1][k1] * B[k1][j1];
-	return clock() - time_mul;						
+    uint64_t time_mul = clock();
+    for (uint64_t i = 0; i < n; i += size_of_block) 
+        for (uint64_t k = 0; k < l; k += size_of_block) 
+	    for (uint64_t j = 0; j < m; j += size_of_block)
+		for (uint64_t i1 = i; i1 < i + size_of_block && i1 < n; i1++)
+	            for (uint64_t k1 = k; k1 < k + size_of_block && k1 < l; k1++)
+			for (uint64_t j1 = j; j1 < j + size_of_block && j1 < m; j1++)
+			    C[i1][j1] += A[i1][k1] * B[k1][j1];
+    return clock() - time_mul;						
 }
 double get_time(char * a, char * b, char * c, char mode, uint64_t size_of_block) {
-	fstream a_f, b_f;
+    fstream a_f, b_f;
     a_f.open(a, ios::binary | ios::in);
     b_f.open(b, ios::binary | ios::in);
     uint64_t N_a, M_a, N_b, M_b;
@@ -70,19 +70,21 @@ double get_time(char * a, char * b, char * c, char mode, uint64_t size_of_block)
     b_f.read((char *)&N_b, sizeof(N_b));
     b_f.read((char *)&M_b, sizeof(M_b));
     fstream c_f(c, ios::binary | ios::out);
-	if (N_b != M_a) throw "Invalid sizes";
-	float ** A = init_matrix(N_a, M_a);
+    if (N_b != M_a) throw "Invalid sizes";
+    float ** A = init_matrix(N_a, M_a);
     read_matrix(A, N_a, M_a, a_f); 
-	float ** B = init_matrix(N_b, M_b);
-	read_matrix(B, N_b, M_b, b_f);
-	float ** C = init_matrix(N_a, M_b); 
-	uint64_t time_mul; 
-	if (mode == 0) time_mul = multiply_ijk(A, B, C, N_a, M_b, N_b, size_of_block, c_f);
-    else  time_mul = multiply_ikj(A, B, C, N_a, M_b, N_b, size_of_block, c_f);
-	delete_matrix(A, N_a);
-	delete_matrix(B, N_b);
-	delete_matrix(C, N_a);
-	return time_mul/(double)CLOCKS_PER_SEC;
+    float ** B = init_matrix(N_b, M_b);
+    read_matrix(B, N_b, M_b, b_f);
+    float ** C = init_matrix(N_a, M_b); 
+    uint64_t time_mul; 
+    if (mode == 0) 
+	time_mul = multiply_ijk(A, B, C, N_a, M_b, N_b, size_of_block, c_f);
+    else  
+	time_mul = multiply_ikj(A, B, C, N_a, M_b, N_b, size_of_block, c_f);
+    delete_matrix(A, N_a);
+    delete_matrix(B, N_b);
+    delete_matrix(C, N_a);
+    return time_mul/(double)CLOCKS_PER_SEC;
 }
 void handle_error(int err){
     std::cerr << "PAPI error: " << err << std::endl;
@@ -107,9 +109,9 @@ int main(int argc, char ** argv) {
         return -1;
     }
     int mode;
-	uint64_t size_of_block;
+    uint64_t size_of_block;
     sscanf(argv[4], "%d", &mode);
-	sscanf(argv[5], "%", SCNd64, &size_of_block); 
+    sscanf(argv[5], "%", SCNd64, &size_of_block); 
     //if ((mode != 0 && mode != 1) || (strcmp(argv[4], "0") !=0 && strcmp(argv[4], "1") != 0) || size_of_block < 1) {
       //  cerr << "Invalid parameters" << endl;
         //return -1;
