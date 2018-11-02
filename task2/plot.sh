8566a8a95ef325128a9a904663bd6599e8fd8fbf
daria <dasha_domracheva@mail.ru>
2018-11-02 13:46:01 +0300

    changed the way of generating graphs

--- task2/plot.sh
+++ task2/plot.sh
@@ -2,22 +2,16 @@
 F1=$1
 F2=$2
 F3=$3
-F4=$4
-F5=$5
-OUT=$6
-TITLE1=$7
-TITLE2=$8
-TITLE3=$9
-TITLE4=${10}
-TITLE5=${11}
+OUT=$4
+TITLE1=$5
+TITLE2=$6
+TITLE3=$7=
 gnuplot -e "
 set terminal svg size 600,500;
 set output './report/$OUT';
-set yrange [0:4];
-set xtics ('32 ijk' 0, '32 ikj' 1, 'opt ijk' 2, 'opt ikj' 3)
+set xrange [0:4];
+set xtics ('1000' 0, '2000' 1, '3000' 2, '4000' 3, '5000' 4)
 plot '$F1' with linespoint title '$TITLE1',\
     '$F2' with linespoint title '$TITLE2',\
-    '$F3' with linespoint title '$TITLE3',\
-    '$F4' with linespoint title '$TITLE4',\
-    '$F5' with linespoint title '$TITLE5'
+    '$F3' with linespoint title '$TITLE3'
 "
--- task2/processdat.sh
+++ task2/processdat.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 F00IN=$1
 F10IN=$2
-F01IN=$3
-F11IN=$4
-OUT=$5
-N=$6
+#F01IN=$3
+F11IN=$3
+OUT00=$4
+OUT10=$5
+OUT11=$6
+N=$7
 #computation of average
-
-> $OUT 
+ 
 cat $F00IN | awk -v num=$N 'BEGIN { coun=1; s=0 } 
-    { while (coun<6) coun++; s+=$num; print s/5 }' >> $OUT
+    { while (coun<6) coun++; s+=$num; } END { print s/5 }' >> $OUT00
 cat $F10IN | awk -v num=$N  'BEGIN { coun=1; s=0 } 
-    { while (coun<6) coun++; s+=$num; print s/5 }' >> $OUT
-cat $F01IN | awk -v num=$N  'BEGIN { coun=1; s=0 } 
-    { while (coun<6) coun++; s+=$num; print s/5 }' >> $OUT
+    { while (coun<6) coun++; s+=$num; } END { print s/5 }' >> $OUT10
+#cat $F01IN | awk -v num=$N  'BEGIN { coun=1; s=0 } 
+  #  { while (coun<6) coun++; s+=$num; } END { print s/5 }' >> $OUT
 cat $F11IN | awk -v num=$N  'BEGIN { coun=1; s=0 } 
-    { while (coun<6) coun++; s+=$num; print s/5 }' >> $OUT
+    { while (coun<6) coun++; s+=$num; } END { print s/5 }' >> $OUT11
