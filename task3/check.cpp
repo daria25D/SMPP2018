ec053d579ad97f3460897e4d7a17ba3a37b11e0a
daria <dasha_domracheva@mail.ru>
2018-11-02 16:06:47 +0300

    added Makefile test

--- task3/check.cpp
+++ task3/check.cpp
@@ -12,12 +12,12 @@ int main(int argc, char ** argv) {
         f >> num;
         int n_sqrt = (int)floor(sqrt(num));
         if (num % 2 == 0 && num != 2) {
-            cout << num << " not prime, error input" << endl;
+            cout << num << " not prime, error input, divided by 2" << endl;
             return -1;
         }
         for (int i = 3; i <= n_sqrt; i += 2) {
             if (num % i == 0) {
-                cout << num << " not prime, error input" << endl;
+                cout << num << " not prime, error input, divided by " << i << endl;
                 return -1;
             }
         }
